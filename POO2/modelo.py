0791bd632fe0972fcc9eeb07f3257314e3b2e7c8
Guilherme Ramos <guiramos027@gmail.com>
2022-07-07 17:27:10 -0300

    Herança POO

--- POO2/modelo.py
+++ POO2/modelo.py
@@ -1,60 +1,45 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+    
+    @property
+    def likes(self):
+        return self._likes
+    
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
-    
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
-
-    def dar_like(self):
-        self.likes += 1
-
-    @property
-    def likes(self):
-        return self.__likes
-    
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
 
 
 vingadores = Filme("Homem Aranha - Sem volta pra casa", 2022, 160)
 
 vingadores.dar_like()
 
-print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Like: {vingadores.likes}")
+print(f"{vingadores.nome} - {vingadores.ano} - {vingadores.duracao}: {vingadores.likes}")
 
 breaking_bad = Serie("Breaking Bad", 2015, 5)
 
 breaking_bad.dar_like()
 breaking_bad.dar_like()
 
-print(f"Nome: {breaking_bad.nome} - Ano: {breaking_bad.ano} - Temporadas: {breaking_bad.temporadas} - Likes {breaking_bad.likes}")
+print(f"{breaking_bad.nome} - {breaking_bad.ano} - {breaking_bad.temporadas}: {breaking_bad.likes}")
