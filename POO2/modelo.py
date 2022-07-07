806055cce393edb88320bebd5913115d8c942a08
Guilherme Ramos <guiramos027@gmail.com>
2022-07-07 16:35:55 -0300

    Private Objects

--- POO2/modelo.py
+++ POO2/modelo.py
@@ -1,22 +1,49 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
     
     def dar_like(self):
-        self.like += 1
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
+        self.__likes = 0
 
     def dar_like(self):
-        self.like += 1
+        self.likes += 1
+
+    @property
+    def likes(self):
+        return self.__likes
+    
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 
 vingadores = Filme("Homem Aranha - Sem volta pra casa", 2022, 160)
