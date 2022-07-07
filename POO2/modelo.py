189ea6efd36e42d3085c8f5dfe008a0bb1f46e37
Guilherme Ramos <guiramos027@gmail.com>
2022-07-07 16:16:49 -0300

    Def Incremental

--- POO2/modelo.py
+++ POO2/modelo.py
@@ -1,18 +1,33 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+    
+    def dar_like(self):
+        self.like += 1
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.like += 1
 
 
 vingadores = Filme("Homem Aranha - Sem volta pra casa", 2022, 160)
-print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}")
+
+vingadores.dar_like()
+
+print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Like: {vingadores.likes}")
 
 breaking_bad = Serie("Breaking Bad", 2015, 5)
-print(f"Nome: {breaking_bad.nome} - Ano: {breaking_bad.ano} - Temporadas: {breaking_bad.temporadas}")
+
+breaking_bad.dar_like()
+breaking_bad.dar_like()
+
+print(f"Nome: {breaking_bad.nome} - Ano: {breaking_bad.ano} - Temporadas: {breaking_bad.temporadas} - Likes {breaking_bad.likes}")
