a8ed94a8b4a40bf892dbed36b1e06575710f2b1e
Guilherme Ramos <guiramos027@gmail.com>
2022-07-08 19:05:38 -0300

    Duck Typing

--- POO2/modelo.py
+++ POO2/modelo.py
@@ -39,13 +39,41 @@ class Serie(Programa):
     def __str__(self):
         return f"{self.nome} - {self.ano} - {self.temporadas} Temporadas - {self.likes} Likes"
 
+class Playlist:
+        def __init__(self, nome, programas):
+            self.nome = nome
+            self._programas = programas
+
+        def __getitem__(self, item):
+            return self._programas[item]
+
+        @property
+        def listagem(self):
+            return self._programas
+        
+        def __len__(self):
+            return len(self._programas)
+
 vingadores = Filme("Homem Aranha - Sem volta pra casa", 2022, 160)
-vingadores.dar_like()
 breaking_bad = Serie("Breaking Bad", 2015, 5)
+tmep = Filme("todo mundo em pânico", 1999, 99)
+vikings = Serie("vikings", 2021, 6)
+
+vingadores.dar_like()
+vingadores.dar_like()
+tmep.dar_like()
+vikings.dar_like()
+vikings.dar_like()
+vikings.dar_like()
+breaking_bad.dar_like()
 breaking_bad.dar_like()
 breaking_bad.dar_like()
 
-filmes_e_series = [vingadores, breaking_bad]
+filmes_e_series = [vingadores, breaking_bad, vikings, tmep]
 
-for programa in filmes_e_series:
+playlist_fim_de_semana = Playlist("fim de semana", filmes_e_series)
+
+print(f"Tamanho da playlist: {len(playlist_fim_de_semana)}")
+
+for programa in playlist_fim_de_semana:
     print(programa)
