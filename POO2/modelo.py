df5a0aa03c6eebc14ed670bdef6a2679c2edb870
Guilherme Ramos <guiramos027@gmail.com>
2022-07-12 10:49:03 -0300

    Herança Multipla

--- POO2/modelo.py
+++ POO2/modelo.py
@@ -46,10 +46,6 @@ class Playlist:
 
         def __getitem__(self, item):
             return self._programas[item]
-
-        @property
-        def listagem(self):
-            return self._programas
         
         def __len__(self):
             return len(self._programas)
