ea9c210ad5f067acbffd8ba3e64b2512e1a995c0
Guilherme Ramos <guiramos027@gmail.com>
2022-11-10 08:15:56 -0300

    Herança de built-in

--- modelo.py
+++ modelo.py
@@ -39,23 +39,32 @@ class Serie(Programa):
     def __str__(self):
         return f"Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporadas} Temporadas - Likes: {self.likes}"
 
-class Playlist(Filme, Serie):
-    def __init__(self, nome, ano, duracao):
-        super().__init__(nome, ano, duracao)
-    pass
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
+    
+    
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
-vingadores.dar_like()
-
-
 spotify = Serie("Som na Faixa", 2022, 2)
+o_irlandes = Filme("o irlândes", 2020, 190)
+vikings = Serie("vikings", 2021, 5)
+
+o_irlandes.dar_like()
+o_irlandes.dar_like()
+o_irlandes.dar_like()
+vikings.dar_like()
+vingadores.dar_like()
 spotify.dar_like()
 spotify.dar_like()
 spotify.dar_like()
 
 
 
-filmes_e_series = [vingadores, spotify]
+filmes_e_series = [vingadores, spotify, vikings, o_irlandes]
 
-for programa in filmes_e_series:
+fim_de_semana = Playlist("Fim de Semana", filmes_e_series)
+
+for programa in fim_de_semana:
     print(programa)
