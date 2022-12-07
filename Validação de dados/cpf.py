ccd6d4e8a71d6a7c323817073ecaed3f3198502a
Guilherme Ramos <guiramos027@gmail.com>
2022-12-07 08:12:56 -0300

    Factory de classes

--- Validação de dados/cpf.py
+++ Validação de dados/cpf.py
@@ -1,24 +1,52 @@
-from validate_docbr import CPF
+from validate_docbr import CPF, CNPJ
 
-class Cpf:
+class Documento():
+
+    @staticmethod
+    def cria_documento(documento):
+        if len(documento) == 11:
+            return DocCpf(documento)
+        elif len(documento) == 14:
+            return DocCnpj(documento)
+        else:
+            raise ValueError("Quantidade de dígitos incorreta!!")
+
+class DocCpf():
     def __init__(self, documento):
-        documento = str(documento)
-        if self.cpf_valido(documento):
+        if self.valida(documento):
             self.cpf = documento
         else:
             raise ValueError("CPF Inválido!!")
 
-    def __str__(self):
-        return self.format_cpf()
+    def __str__(self) -> str:
+        return self.format()
 
-    def cpf_valido(self, cpf):
-        if len(cpf) == 11:
-            validador = CPF()
-            return validador.validate(cpf)
+
+    def valida(self, documento):
+        validador = CPF()
+        return validador.validate(documento)
+
+    def format(self):
+        formatado = CPF()
+        return formatado.mask(self.cpf)
+
+
+class DocCnpj():
+    def __init__(self, documento):
+        if self.valida(documento):
+            self.cnpj = documento
         else:
-            raise ValueError("Quantidade de dígitos Inválido")
+            raise ValueError("CNPJ Inválido!!")
+
+    def __str__(self) -> str:
+        return self.format()
 
 
-    def format_cpf(self):
-        cpf_formatado = CPF()
-        return cpf_formatado.mask(self.cpf)
+    def valida(self, documento):
+        validador = CNPJ()
+        return validador.validate(documento)
+
+    def format(self):
+        formatado = CNPJ()
+        return formatado.mask(self.cnpj)
+
--- Validação de dados/main.py
+++ Validação de dados/main.py
@@ -1,6 +1,12 @@
-from cpf import Cpf
+from cpf import Documento
+from validate_docbr import CNPJ
 
 
-cpf = Cpf("10633406910")
+
+
+cpf = Documento.cria_documento("10633406910")
 print(cpf)
 
+exemplo_cnpj = "35379838000112"
+documento = Documento.cria_documento(exemplo_cnpj)
+print(documento)
